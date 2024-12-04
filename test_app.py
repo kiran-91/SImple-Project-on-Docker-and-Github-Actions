6b8f6de81ab2c693bf0c3267aafe6921841a8add
Kiran Deshpande <deshpandek@outlook.com>
2024-12-04 18:15:00 +0530

    modified yaml file

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ from app import app
 
 # we need to start the function name with test 
 def test_home():
-    response=app.test_client(),get("/")
+    response=app.test_client().get("/")
     
     assert response.status_code==200
     assert response.data==b"Hello World!"
